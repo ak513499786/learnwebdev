4240f110e2bbfb8515c235ee591947ea9926f762
Parallel-EDU <ouremail@example.com>
2024-08-16 19:35:32 +0530

    hero fixed

--- src/components/blogPages/tags/hero.jsx
+++ src/components/blogPages/tags/hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
           : "text-[#e0e0e0] bg-[#121212]"
         } px-[80px] max-md:px-[40px] max-sm:px-[20px] h-[650px] max-sm:pt-[180px] relative pt-[250px]`}
         >
-      <h1 className=" text-[92px] max-lg:text-[76px] w-[1024px] max-md:text-[62px] max-sm:text-[56px] font-semibold leading-[120%]">
+      <h1 className=" text-[92px] max-lg:w-full max-lg:text-[76px] w-[1024px] max-md:text-[62px] max-sm:text-[56px] font-semibold leading-[120%]">
         Different Types of HTML Tags{" "}
       </h1>
     </main>
